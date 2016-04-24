1659dbff8bf7eacff7bbfdd0db93ef9aa39d456f
Tejas Mehta <Tejaslog@togmb.local>
2016-04-24 11:27:07 -0700

    Initial Commit

---
+++ Innovative Calculator for iOS9/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Innovative Calculator for iOS9
+//
+//  Created by Tejas Mehta on 4/24/16.
+//  Copyright © 2016 Tejas Mehta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
